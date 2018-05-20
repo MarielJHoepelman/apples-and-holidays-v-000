2bc351459914a3e1ad771a13efae7936465421ba
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-05-20 18:07:47 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,9 +21,9 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash.each do |seasons, holidays|
+  holiday_hash.each do |season, holidays|
   end
-  
+
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
@@ -64,10 +64,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
